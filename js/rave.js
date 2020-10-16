5fe21d19b187acf26d19e5e966e94c73465f7539
Tonva07 <teva@2gik.ch>
2020-10-16 11:59:34 +0200

    wd

--- js/rave.js
+++ js/rave.js
@@ -1,6 +1,10 @@
 // Rave Scripts
 
 // Pre-loader animation
+$(document).ready(function(){
+    $(this).scrollTop(0);
+});
+
 $(window).on('load',function(){
     $('.pre-loader').fadeOut(1000);
     $('body').css('overflow','scroll');
