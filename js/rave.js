1dbfea443300391737699b60f03edcb7673df8f9
Tonva07 <teva@2gik.ch>
2020-10-16 11:25:55 +0200

    Update rave.js

--- js/rave.js
+++ js/rave.js
@@ -1,6 +1,7 @@
 // Rave Scripts
+
+// Pre-loader animation
 $(window).on('load',function(){
     $('.pre-loader').fadeOut(1000);
     setTimeout(function(){}, 1000);
-    $('.pre-loader').remove();
 })
